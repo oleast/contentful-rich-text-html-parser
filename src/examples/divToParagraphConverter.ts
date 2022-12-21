73c893947603548ba2a995550779a47b2e0e19f4
oleast <ole.stokker@bekk.no>
2022-12-21 12:29:32 +0100

    feat(readme): add a readme to the project

---
+++ src/examples/divToParagraphConverter.ts
@@ -0,0 +1,36 @@
+import { Block, BLOCKS } from "@contentful/rich-text-types";
+import { htmlStringToDocument, Options, TagConverter } from "../index";
+
+const divToParagraphConverter: TagConverter<Block> = (node, next) => {
+  return {
+    nodeType: BLOCKS.PARAGRAPH,
+    content: next(node),
+    data: {},
+  };
+};
+
+const options: Options = {
+  convertTag: {
+    div: divToParagraphConverter,
+  },
+};
+
+const htmlString = `<div>Text in a div!</div>`;
+
+htmlStringToDocument(htmlString, options);
+
+// {
+//   nodeType: 'document',
+//   content: [
+//     {
+//       nodeType: 'paragraph',
+//       content: [
+//         {
+//           nodeType: 'text',
+//           value: 'Text in a div!',
+//           marks: [],
+//         },
+//       ],
+//     },
+//   ],
+// };
